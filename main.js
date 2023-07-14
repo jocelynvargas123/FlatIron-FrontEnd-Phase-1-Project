e12e55dec511c90b3bdc8ee343f3adfbea2700e1
Jocelyn <jocelynvargas123@yahoo.com>
2023-07-14 00:45:39 -0700

    "plant list fixed"

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 console.log('connected')
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('http://localhost:3000/plants')
+    fetch('http://localhost:3000/Plants')
     .then(res => res.json())
-    return console.log("yay")
+    .then(plantInfo => plantInfo.forEach(plant => createPlantCard(plant)))
 })
+
+function createPlantCard (plant){
+    let card = document.createElement('div')
+        card.classList.add('plantCard')
+        let h3 = document.createElement('h3')
+        h3.textContent = `Plant name: ${plant.name} `
+        let img = document.createElement('img')
+        img.src = plant.img
+        img.classList.add('plant-photo')
+        let p = document.createElement('p')
+        p.textContent = `Information: ${plant.info}`
+        let p2 = document.createElement('p')
+        p2.textContent = `Date: ${plant.date}`
+        let btn = document.createElement('button')
+        btn.textContent =  `Water in ${plant.days} days `
+        
+    card.append(h3,img,p,p2,btn)
+    document.getElementById('plantList').appendChild(card)
+}
